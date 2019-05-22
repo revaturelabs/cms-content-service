f7587c30f80f1a0c813c738724a3fdc5a8cb2e3c
J D <john_david_orr@yahoo.com>
2019-05-22 16:01:02 -0400

    Needs more work on the ContentModuleEntityTest

--- CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
+++ CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
@@ -2,12 +2,23 @@ package com.revature.cmsforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories("com.revature.repositories")
+@EntityScan("com.revature.entities")
+@ComponentScan("com.revature")
 public class CMSforceApplication {
-
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CMSforceApplication.class, args);
+		
+	
+		
 	}
 
 }
