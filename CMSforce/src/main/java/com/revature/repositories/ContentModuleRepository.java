2d678fd603cbaf98879a8e6fad6bcdd42d9def68
J D <john_david_orr@yahoo.com>
2019-05-22 14:03:03 -0400

    Putting in the repositories

---
+++ CMSforce/src/main/java/com/revature/repositories/ContentModuleRepository.java
@@ -0,0 +1,11 @@
+package com.revature.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.entities.ContentModule;
+
+@Repository
+public interface ContentModuleRepository extends CrudRepository <ContentModule, Integer>{
+
+}
---
+++ CMSforce/src/main/java/com/revature/repositories/ContentRepository.java
@@ -0,0 +1,11 @@
+package com.revature.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.entities.Content;
+
+@Repository
+public interface ContentRepository extends CrudRepository <Content, Integer> {
+
+}
