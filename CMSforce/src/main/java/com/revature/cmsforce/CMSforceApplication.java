a9d9394c0e3d384e5a54a1515d7d7b8633a62389
Adam_Ranieri <adam.ranieri@revature.com>
2019-05-22 11:23:14 -0400

    added base project

---
+++ CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.cmsforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMSforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMSforceApplication.class, args);
+	}
+
+}
