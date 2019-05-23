473c672ba89ddd5a56216ccb53965ded6b8780e8
willtring96 <willtring96@yahoo.com>
2019-05-23 08:55:38 -0400

    Cloning

--- CMSforce/src/main/java/com/revature/entities/ContentModule.java
+++ CMSforce/src/main/java/com/revature/entities/ContentModule.java
@@ -1,9 +1,16 @@
 package com.revature.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "content_module")
@@ -40,7 +47,7 @@ public class ContentModule {
 	private List<Module> modules = new ArrayList<Module>();
 
 	@OneToMany (cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Content> contents = ArrayList<Content>();
+	private List<Content> contents = new ArrayList<Content>();
 
 	public int getId() {
 		return id;
