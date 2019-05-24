46c4e2fa44b304dc27865f8ea9b6a97f495b43f8
Mauricio Azcona <azcona.mauricio@gmail.com>
2019-05-24 14:23:04 -0400

    Refactored RepositoriesSmokeTests test names and added random string generator to ModuleEntitySaveTest

--- CMSforce/src/test/java/com/revature/smoketests/RepositoriesSmokeTests.java
+++ CMSforce/src/test/java/com/revature/smoketests/RepositoriesSmokeTests.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -23,88 +25,98 @@ import com.revature.repositories.ContentRepository;
 import com.revature.repositories.ModuleRepository;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes=com.revature.cmsforce.CMSforceApplication.class)
+@ContextConfiguration(classes = com.revature.cmsforce.CMSforceApplication.class)
 @SpringBootTest
 class RepositoriesSmokeTests {
 
 	@Autowired
 	ModuleRepository mr;
-	
+
 	@Autowired
 	ContentRepository cr;
-	
+
 	@Autowired
 	ContentModuleRepository cmr;
-	
+
 	@Test
-	public void ModuleEntitySave() {
+	public void ModuleEntitySaveTest() {
 		Module m = new Module();
-		m.setSubject("JavaScript");
+
+		int leftLimit = 97; // letter 'a'
+		int rightLimit = 122; // letter 'z'
+		Random random = new Random();
+		int targetStringLength = random.nextInt(20);
+		StringBuilder buffer = new StringBuilder(targetStringLength);
+		for (int i = 0; i < targetStringLength; i++) {
+			int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+			buffer.append((char) randomLimitedInt);
+		}
+		String generatedString = buffer.toString();
+
+		m.setSubject(generatedString);
 		mr.save(m);
 	}
+
 	@Test
-	public void ContentEntitySave() {
+	public void ContentEntitySaveTest() {
 		Content c = new Content();
 		c.setDescription("Description");
 		c.setFormat("format");
 		c.setTitle("Title");
 		c.setUrl("URI");
 		cr.save(c);
-		
+
 	}
-	
+
 	@Test
-	public void getContentByTitle() {
+	public void getContentByTitleTest() {
 		Set<Content> contents = null;
 		contents = cr.findByTitle("Title");
-		List<Content> allContents=new ArrayList<Content>();
+		List<Content> allContents = new ArrayList<Content>();
 		cr.findAll().forEach(allContents::add);
-		allContents.stream()
-			.filter((cont)->cont.getTitle().equals("Title"))
-			.collect(Collectors.toList());
-		assertEquals(allContents.size(),contents.size(), "Number of results is equal");
+		allContents.stream().filter((cont) -> cont.getTitle().equals("Title")).collect(Collectors.toList());
+		assertEquals(allContents.size(), contents.size(), "Number of results is equal");
 	}
+
 	@Test
-	public void getContentByFormat() {
+	public void getContentByFormatTest() {
 		Set<Content> contents = null;
 		contents = cr.findByFormat("format");
 		List<Content> allContents = new ArrayList<Content>();
 		cr.findAll().forEach(allContents::add);
-		allContents.stream()
-			.filter((cont)->cont.getFormat().equals("format"))
-		.collect(Collectors.toList());
-		assertEquals(allContents.size(),contents.size(),"Number of results is equal");
-		
-		
+		allContents.stream().filter((cont) -> cont.getFormat().equals("format")).collect(Collectors.toList());
+		assertEquals(allContents.size(), contents.size(), "Number of results is equal");
+
 	}
+
 	@Test
-	public void getModuleBySubject() {
-		Set<Module> modules = null;
-		modules = mr.findBysubject("JavaScript");
-		List<Module> allModules = new ArrayList<Module>();
+	public void getModuleBySubjectTest() {
+		Set<Module> allModules = new HashSet<Module>();
 		mr.findAll().forEach(allModules::add);
-		allModules.stream()
-			.filter((cont)->cont.getSubject().equals("JavaScript"))
-		.collect(Collectors.toList());
-		assertEquals(allModules.size(),modules.size(),"Number of results is equal");
-		
-		
+
+		Iterator<Module> iter = allModules.iterator();
+		Module first = iter.next();
+
+		Set<Module> modules = mr.findBysubject(first.getSubject());
+
+		assertEquals(modules.size(), 1);
+
 	}
+
 	@Test
-	public void ContentModuleEntitySave() {
+	public void ContentModuleEntitySaveTest() {
 		ContentModule cm = new ContentModule();
-		
+
 		Module js = mr.findById(6).get();
 		Content cont = cr.findById(5).get();
 		Set<Module> modules = new HashSet<Module>();
 		Set<Content> contents = new HashSet<Content>();
 		modules.add(js);
 		contents.add(cont);
-		//cm.setContents(contents);
-		//cm.setModules(modules);
+		// cm.setContents(contents);
+		// cm.setModules(modules);
 		cmr.save(cm);
-		
+
 	}
 
 }
-
