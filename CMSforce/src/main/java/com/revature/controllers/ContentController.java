e94dff13861de832d774b9ae8a449bcb7d330f5e
NicholasFittipaldi <nfittutc@gmail.com>
2019-05-24 12:48:22 -0400

    added content controller

--- CMSforce/src/main/java/com/revature/controllers/ContentController.java
+++ CMSforce/src/main/java/com/revature/controllers/ContentController.java
@@ -29,44 +29,50 @@ public class ContentController {
 	@Autowired
 	ModuleService moduleService;
 	
+	// 
 	@RequestMapping(value = "/content", method = RequestMethod.POST) 
-	public Content createContent(@RequestParam("newContent") Content content, @RequestParam("modules") String[] moduleNames) {
-		Content createdContent = contentService.createContent(content);
-		
-		
-		
-		return createdContent;
+	public Content createContent(@RequestParam("newContent") Content content, @RequestParam("modules") ContentModule[] modules) {
+//		Content createdContent = contentService.createContent(content);
+		contentService.addContentAndContentModule(content, modules);
+		return content;
 	}
 	
+	// Returns a set of contents 
+	// Finds all the content in the repository 
 	@RequestMapping(value = "/content", method = RequestMethod.GET)
 	public Set<Content> getAllContent() {
 		return contentService.getAllContent();
 	}
 	
+	// Returns content depending on what ID is passed in
+	// Uses the findById method in the repository
 	@RequestMapping(value = "/content/{id}", method = RequestMethod.GET)
 	public Content getContentById(@PathVariable int id) {
 		return contentService.getContentById(id);
 	}
 	
+	// Updates a specific content in the table 
+	// First assures that the content exists by its ID and if it does
+	// it will update that content
 	@RequestMapping(value = "/content", method = RequestMethod.PUT)
 	public Content updateContent(@RequestBody Content inputContent) {
 		return contentService.updateContent(inputContent);
 	}
 	
-	@RequestMapping(value = "/content/{id}/modules", method = RequestMethod.PUT)
+//	@RequestMapping(value = "/content/{id}/modules", method = RequestMethod.PUT)
 //	public Content addContentModules(@PathVariable int id, int[] moduleIds) {
-	public Content addContentModules(@PathVariable int id, ContentModule[] contentModules) {
-		Content existingContent = contentService.getContentById(id);
-		List<Module> modulesToAdd = new ArrayList<Module>();
-
+//	public Content addContentModules(@PathVariable int id, ContentModule[] contentModules) {
+//		Content existingContent = contentService.getContentById(id);
+//		List<Module> modulesToAdd = new ArrayList<Module>();
+//
 //		for (int moduleId : moduleIds) {
 //			modulesToAdd.add(moduleService.getModuleById(moduleId));
 //		}
-		
-		for (ContentModule contentModule : contentModules) {
-			modulesToAdd.add(moduleService.getModuleById(contentModule.getFkModule()));
-		}
-		
-		return contentService.addContentModules(existingContent, modulesToAdd.toArray(new Module[0]));
-	}
+//		
+//		for (ContentModule contentModule : contentModules) {
+//			modulesToAdd.add(moduleService.getModuleById(contentModule.getFkModule()));
+//		}
+//		
+//		return contentService.addContentModules(existingContent, modulesToAdd.toArray(new Module[0]));
+//	}
 }
