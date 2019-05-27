9889124d7f2ac6d53673b202c35d23915cbb1fad
J D <john_david_orr@yahoo.com>
2019-05-27 16:17:02 -0400

    Fixed YML to be the other database and also refactored revatire to revature

--- CMSforce/src/test/java/com/revature/controllertests/ContentControllerTest.java
+++ CMSforce/src/test/java/com/revature/controllertests/ContentControllerTest.java
@@ -1,4 +1,4 @@
-package com.revatire.controllertests;
+package com.revature.controllertests;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
