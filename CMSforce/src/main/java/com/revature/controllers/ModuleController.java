e0c9784f1fbf042afbc770c78c0a0cd328a87623
J D <john_david_orr@yahoo.com>
2019-06-03 12:42:18 -0400

    fixing controllers

--- CMSforce/src/main/java/com/revature/controllers/ModuleController.java
+++ CMSforce/src/main/java/com/revature/controllers/ModuleController.java
@@ -4,7 +4,9 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,17 +22,17 @@ public class ModuleController {
 	@Autowired
 	ModuleService moduleService;
 	
-	@RequestMapping(value = "/module", method = RequestMethod.GET)
+	@GetMapping
 	public Set<Module> getAllModules() {
 		return moduleService.getAllModules();
 	}
 	
-	@RequestMapping(value = "/module/{id}", method = RequestMethod.GET)
+	@GetMapping
 	public Module getModuleById(@PathVariable int id) {
 		return moduleService.getModuleById(id);
 	}
 	
-	@RequestMapping(value = "/module", method = RequestMethod.POST)
+	@PostMapping
 	public Module createModule(@RequestBody Module module) {
 		return moduleService.createModule(module);
 	}
