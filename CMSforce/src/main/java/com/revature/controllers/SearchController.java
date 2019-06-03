04eefdd42ff1123f03dd5d95d340af2d61a808ca
DJ <djek111692@gmail.com>
2019-06-03 14:13:58 -0400

    adding @Transactional to controller

--- CMSforce/src/main/java/com/revature/controllers/SearchController.java
+++ CMSforce/src/main/java/com/revature/controllers/SearchController.java
@@ -6,11 +6,10 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.entities.Content;
@@ -18,6 +17,7 @@ import com.revature.services.SearchService;
 
 @CrossOrigin(origins = "*", allowCredentials="true")
 @RestController
+@Transactional
 public class SearchController {
 
 	@Autowired
@@ -25,6 +25,7 @@ public class SearchController {
 	
 	@PostMapping("/search")
 	public Set<Content> filter(@RequestBody Map<String, Object> body) {
+		@SuppressWarnings("unchecked")
 		List<Integer> lst = (ArrayList<Integer>) body.get("modules");
 		return searchService.filter(body.get("title").toString(), body.get("format").toString(), lst);
 	}
