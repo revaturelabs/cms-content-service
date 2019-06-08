f741a89f3a00488b550bbf7bddbc5d84aa373e79
J D <john_david_orr@yahoo.com>
2019-06-08 11:15:35 -0400

    added microservices

--- CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
+++ CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
@@ -1,8 +1,10 @@
 package com.revature.cmsforce;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.revature.repositories")
 @EntityScan("com.revature.entities")
 @ComponentScan("com.revature")
+@EnableEurekaClient
+@EnableAutoConfiguration
 public class CMSforceApplication {
 	
 	
