167514912760522b3571bf3b8812a82ce4aff387
J D <john_david_orr@yahoo.com>
2019-06-08 01:09:54 -0400

    lol

--- CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
+++ CMSforce/src/main/java/com/revature/cmsforce/CMSforceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.revature.entities")
 @ComponentScan("com.revature")
 @EnableEurekaClient
+@EnableZuulProxy
 public class CMSforceApplication {
 	
 	
