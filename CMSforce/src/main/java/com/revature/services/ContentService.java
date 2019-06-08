a535cf662507c24d80ec6318d17d9c20fe605ef3
J D <john_david_orr@yahoo.com>
2019-06-07 23:33:06 -0400

    one line

--- CMSforce/src/main/java/com/revature/services/ContentService.java
+++ CMSforce/src/main/java/com/revature/services/ContentService.java
@@ -3,7 +3,7 @@ import java.util.Set;
 import com.revature.entities.Content;
 public interface ContentService {
 	
-    public Content createContent(Content content) throws Exception;
+    public Content createContent(Content content);
     
     public Set<Content> getAllContent();
     
