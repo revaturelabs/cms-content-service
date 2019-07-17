ba46866f9a02e7ae8e546ddb892da69e67f3452c
pacquito <paco.felepia@gmail.com>
2019-07-17 14:16:33 -0500

    changed application yml and first test

---
+++ src/test/java/com/revature/servicestests/TimeGraphServiceTest.java
@@ -0,0 +1,43 @@
+package com.revature.servicestests;
+
+import java.util.HashSet;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.entities.Content;
+import com.revature.entities.Link;
+import com.revature.services.ContentService;
+import com.revature.services.TimegraphService;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = com.revature.cmsforce.CMSforceApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Transactional
+@SpringBootTest
+public class TimeGraphServiceTest {
+	
+	@Autowired
+	TimegraphService ts;
+	
+	@Autowired
+	ContentService cs;
+	
+	@Test
+	@Commit
+	@Order(1)
+	void createValidContent() {
+		cs.createContent(new Content(0, "FIRST TEST CONTENT", "Code", "FIRST TEST CONTENT DESCRIPTION",
+				"http://TESTURL.COM", new HashSet<Link>(), 1563378565, 1563378565));
+	}
+
+}
