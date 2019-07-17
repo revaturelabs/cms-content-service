e3492ee8dd64395b203ac3568632ba8c73589fad
Allen Rankin <aarankin@olivet.edu>
2019-07-17 11:42:05 -0500

    Added Metrics Controller class to start calculating metrics server-side

---
+++ src/main/java/com/revature/controllers/MetricsController.java
@@ -0,0 +1,23 @@
+package com.revature.controllers;
+
+public class MetricsController {
+	// reports mapping for metrics
+	// get all first then do parsing as called 
+	
+	
+	
+	// code
+	
+	
+	
+	// documents 
+	
+	
+	
+	
+	//powerpoints
+	
+	
+	
+	// avg num resources 
+}
