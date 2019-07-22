42aea83fae925f4074b978d11b5a5840bd79dec3
Matthew Do <mattdo290@gmail.com>
2019-07-22 15:15:48 -0500

    removed unused imports

--- src/main/java/com/revature/controllers/ContentController.java
+++ src/main/java/com/revature/controllers/ContentController.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
