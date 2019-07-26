10cbc572a446bec794fadf8adef7618c1d73bfe3
Mantu Nguyen <mantu@Mantus-MacBook-Pro.local>
2019-07-26 07:45:16 -0500

    remove unused import

--- src/main/java/com/revature/util/ServiceLogAspect.java
+++ src/main/java/com/revature/util/ServiceLogAspect.java
@@ -6,8 +6,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.revature.entities.Module;
-
 @Component
 @Aspect
 public class ServiceLogAspect {
