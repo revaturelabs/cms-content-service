dcad882d550579d819ae46af2eb466da592f3188
DannieJWright <Danniejw@gmail.com>
2019-08-18 08:56:13 -0500

    Adds search controller tests

---
+++ src/test/java/com/revature/controllertests/ModuleControllerTest.java
@@ -0,0 +1,12 @@
+package com.revature.controllertests;
+
+import org.testng.annotations.Test;
+
+public class ModuleControllerTest {
+	
+	//checkout Mockitos verify() to verify the service method was called
+	
+  @Test
+  public void f() {
+  }
+}
