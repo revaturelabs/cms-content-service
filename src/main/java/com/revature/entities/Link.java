5f3aa65f600ebc75cd4edb3810cd426bad5471ba
Kenny Bui <kbui3145@gmail.com>
2019-08-27 13:53:49 -0500

    Added changes to entities

--- src/main/java/com/revature/entities/Link.java
+++ src/main/java/com/revature/entities/Link.java
@@ -3,6 +3,7 @@ package com.revature.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,7 +11,7 @@ import javax.persistence.Table;
 @Table(name = "link")
 public class Link {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "cm_id")
 	private int id;
 
--- src/main/java/com/revature/entities/ModuleHierarchy.java
+++ src/main/java/com/revature/entities/ModuleHierarchy.java
@@ -3,6 +3,7 @@ package com.revature.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -11,7 +12,7 @@ import javax.persistence.Table;
 public class ModuleHierarchy {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "j_id")
 	private int id;
 
