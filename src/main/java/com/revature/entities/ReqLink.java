fa5b2fcea3790ec4a5fc3f1508a097bf81aea090
Josh Pontipiedra <joshpontipiedra@gmail.com>
2019-09-14 16:52:13 -0400

    thing

--- src/main/java/com/revature/entities/ReqLink.java
+++ src/main/java/com/revature/entities/ReqLink.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "reqLink")
+@Table(name = "req_link")
 public class ReqLink {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
