b4b79b5d6fad1bb6250ab64aaa8af9d8ea49f4a9
Samuel Dorilas <samueldorilas@outlook.com>
2019-10-12 17:45:48 -0400

    Added full coverage for LinkControllerTest. Added MetricsControllerTest to testNGsuite.xml since it was missing.

--- src/test/java/com/revature/controllertests/LinkControllerTest.java
+++ src/test/java/com/revature/controllertests/LinkControllerTest.java
@@ -22,7 +22,9 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static org.mockito.ArgumentMatchers.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -33,134 +35,142 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = CMSforceApplication.class)
-public class LinkControllerTest
-{
+public class LinkControllerTest {
 
-    private MockMvc mvc;
-    private ObjectMapper objMapper = new ObjectMapper();
-    @InjectMocks
-    private LinkController lc;
-    @Mock
-    private LinkService ls;
+	private MockMvc mvc;
+	private ObjectMapper objMapper = new ObjectMapper();
+	@InjectMocks
+	private LinkController lc;
+	@Mock
+	private LinkService ls;
 
-    @BeforeClass
-    public void setup()
-    {
-        lc = new LinkController();
-        mvc = MockMvcBuilders.standaloneSetup(lc).build();
-    }
+	@BeforeClass
+	public void setup() {
+		lc = new LinkController();
+		mvc = MockMvcBuilders.standaloneSetup(lc).build();
+	}
 
-    @BeforeMethod
-    public void reinitMocks()
-    {
-        MockitoAnnotations.initMocks(this);
-    }
+	@BeforeMethod
+	public void reinitMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-    @Test
-    public void testCreateLink() throws Exception
-    {
-        Content content = new Content(1, "title", "format", "description", "http://www.google.com",
-                1L, 1L, new HashSet<Link>());
-        Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
-                new HashSet<Module>(), new HashSet<Module>());
-        Link link = new Link(0, content, module, "affiliation");
-        Mockito.when(ls.createLink(link)).thenReturn(link);
+	@Test
+	public void testCreateLink() throws Exception {
+		Content content = new Content(1, "title", "format", "description", "http://www.google.com", 1L, 1L,
+				new HashSet<Link>());
+		Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
+				new HashSet<Module>(), new HashSet<Module>());
+		Link link = new Link(0, content, module, "affiliation");
+		Mockito.when(ls.createLink(link)).thenReturn(link);
 
-        ResultActions result = mvc.perform( post("/links")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(objMapper.writeValueAsString(link)));
+		ResultActions result = mvc.perform(post("/links").contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(objMapper.writeValueAsString(link)));
 
-        Link actual = objMapper.readValue(result.andReturn().getResponse()
-                .getContentAsString(), Link.class);
+		Link actual = objMapper.readValue(result.andReturn().getResponse().getContentAsString(), Link.class);
 
-        result.andExpect(status().isOk());
-        Mockito.verify(ls).createLink(link);
-        Assert.assertEquals(actual, link, "Failed to retrieve expected link");
-    }
+		result.andExpect(status().isOk());
+		Mockito.verify(ls).createLink(link);
+		Assert.assertEquals(actual, link, "Failed to retrieve expected link");
+	}
 
-    @Test
-    public void testGetAllLinks() throws Exception
-    {
-        Content content = new Content(1, "title", "format", "description", "http://www.google.com",
-                1L, 1L, new HashSet<Link>());
-        Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
-                new HashSet<Module>(), new HashSet<Module>());
-        Link link = new Link(0, content, module, "affiliation");
-        Set<Link> allLinks = new HashSet<Link>();
-        allLinks.add(link);
+	@Test
+	public void testGetAllLinks() throws Exception {
+		Content content = new Content(1, "title", "format", "description", "http://www.google.com", 1L, 1L,
+				new HashSet<Link>());
+		Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
+				new HashSet<Module>(), new HashSet<Module>());
+		Link link = new Link(0, content, module, "affiliation");
+		Set<Link> allLinks = new HashSet<Link>();
+		allLinks.add(link);
 
-        Mockito.when(ls.getAllLinks()).thenReturn(allLinks);
+		Mockito.when(ls.getAllLinks()).thenReturn(allLinks);
 
-        ResultActions result = mvc.perform( get("/links")
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+		ResultActions result = mvc.perform(get("/links").contentType(MediaType.APPLICATION_JSON_VALUE));
+		System.out.println(result.andReturn().getResponse().getContentAsString());
+		System.out.println("lklk");
+		Set<Link> actual = objMapper.readValue(result.andReturn().getResponse().getContentAsString(),
+				new TypeReference<Set<Link>>() {
+				});
 
-        Set<Link> actual = objMapper.readValue(result.andReturn().getResponse()
-                .getContentAsString(), new TypeReference<Set<Link>>() { });
+		result.andExpect(status().isOk());
+		Mockito.verify(ls).getAllLinks();
+		Assert.assertEquals(actual, allLinks, "Failed to retrieve expected links");
+	}
 
-        result.andExpect(status().isOk());
-        Mockito.verify(ls).getAllLinks();
-        Assert.assertEquals(actual, allLinks, "Failed to retrieve expected links");
-    }
+	@Test
+	public void testGetLinkById() throws Exception {
+		Content content = new Content(1, "title", "format", "description", "http://www.google.com", 1L, 1L,
+				new HashSet<Link>());
+		Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
+				new HashSet<Module>(), new HashSet<Module>());
+		Link link = new Link(1, content, module, "affiliation");
+		Mockito.when(ls.getLinkById(link.getId())).thenReturn(link);
 
-    @Test
-    public void testGetLinkById() throws Exception
-    {
-        Content content = new Content(1, "title", "format", "description", "http://www.google.com",
-                1L, 1L, new HashSet<Link>());
-        Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
-                new HashSet<Module>(), new HashSet<Module>());
-        Link link = new Link(1, content, module, "affiliation");
-        Mockito.when(ls.getLinkById(link.getId())).thenReturn(link);
+		ResultActions result = mvc.perform(get("/links/" + link.getId()).contentType(MediaType.APPLICATION_JSON_VALUE));
 
-        ResultActions result = mvc.perform( get("/links/" + link.getId())
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+		Link actual = objMapper.readValue(result.andReturn().getResponse().getContentAsString(), Link.class);
 
-        Link actual = objMapper.readValue(result.andReturn().getResponse()
-                .getContentAsString(), Link.class);
+		result.andExpect(status().isOk());
+		Mockito.verify(ls).getLinkById(link.getId());
+		Assert.assertEquals(actual, link, "Failed to retrieve expected link");
+	}
 
-        result.andExpect(status().isOk());
-        Mockito.verify(ls).getLinkById(link.getId());
-        Assert.assertEquals(actual, link, "Failed to retrieve expected link");
-    }
+	@SuppressWarnings("unchecked")
+	@Test()
+	public void testGetSearchResults() throws Exception {
+		String[] params = { "title", "format", "modules" };
+		Content content = new Content(1, "title", "format", "description", "http://www.google.com", 1L, 1L,
+				new HashSet<Link>());
+		Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
+				new HashSet<Module>(), new HashSet<Module>());
+		Link link = new Link(0, content, module, "affiliation");
+		Set<Link> allLinks = new HashSet<Link>();
+		allLinks.add(link);
+		Set<Set<Link>> setOfSets = new HashSet<>();
+		setOfSets.add(allLinks);
+		
+		Mockito.when(ls.filter(anyString(), anyString(), any(ArrayList.class))).thenReturn(setOfSets);
 
-    @Test(enabled=false)
-    public void testGetSearchResults()
-    {
-        // Needs implementation
-    }
+		ResultActions result = mvc
+				.perform(get("/links").param(params[0], content.getTitle()).param(params[1], content.getFormat())
+						.param(params[2], "1"));
+		Set<Set<Link>> actual = objMapper.readValue(result.andReturn().getResponse().getContentAsString(),
+				new TypeReference<Set<Set<Link>>>() {
+				});
 
-    @Test
-    public void testUpdateLink() throws Exception
-    {
-        Content content = new Content(1, "title", "format", "description", "http://www.google.com",
-                1L, 1L, new HashSet<Link>());
-        Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
-                new HashSet<Module>(), new HashSet<Module>());
-        Link link = new Link(1, content, module, "affiliation");
-        Mockito.when(ls.updateLink(link)).thenReturn(link);
+		result.andExpect(status().isOk());
+		Mockito.verify(ls).filter(anyString(), anyString(), any(ArrayList.class));
+		Assert.assertEquals(actual, setOfSets, "Failed to retrieve expected links");
+	}
 
-        ResultActions result = mvc.perform( put("/links/" + link.getId())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(objMapper.writeValueAsString(link)));
+	@Test
+	public void testUpdateLink() throws Exception {
+		Content content = new Content(1, "title", "format", "description", "http://www.google.com", 1L, 1L,
+				new HashSet<Link>());
+		Module module = new Module(1, "test_module", 1L, new HashSet<Link>(), new HashSet<ReqLink>(),
+				new HashSet<Module>(), new HashSet<Module>());
+		Link link = new Link(1, content, module, "affiliation");
+		Mockito.when(ls.updateLink(link)).thenReturn(link);
 
-        Link actual = objMapper.readValue(result.andReturn().getResponse()
-                .getContentAsString(), Link.class);
+		ResultActions result = mvc.perform(put("/links/" + link.getId()).contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(objMapper.writeValueAsString(link)));
 
-        result.andExpect(status().isOk());
-        Mockito.verify(ls).updateLink(link);
-        Assert.assertEquals(actual, link, "Failed to retrieve expected link");
-    }
+		Link actual = objMapper.readValue(result.andReturn().getResponse().getContentAsString(), Link.class);
 
-    @Test
-    public void testDeleteLink() throws Exception
-    {
-        int linkId = 1;
-        Mockito.doNothing().when(ls).deleteLinkById(linkId);
+		result.andExpect(status().isOk());
+		Mockito.verify(ls).updateLink(link);
+		Assert.assertEquals(actual, link, "Failed to retrieve expected link");
+	}
 
-        ResultActions result = mvc.perform( delete("/links/" + linkId)
-                .contentType(MediaType.APPLICATION_JSON_VALUE));
+	@Test
+	public void testDeleteLink() throws Exception {
+		int linkId = 1;
+		Mockito.doNothing().when(ls).deleteLinkById(linkId);
 
-        result.andExpect(status().isOk());
-        Mockito.verify(ls).deleteLinkById(linkId);
-    }
+		ResultActions result = mvc.perform(delete("/links/" + linkId).contentType(MediaType.APPLICATION_JSON_VALUE));
+
+		result.andExpect(status().isOk());
+		Mockito.verify(ls).deleteLinkById(linkId);
+	}
 }
