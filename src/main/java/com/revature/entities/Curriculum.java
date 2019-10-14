1f0623dba23acfefa1cc01c6224d6c7dd3e38c8a
Kyle Reimer <reimerkp@gmail.com>
2019-10-14 13:18:07 -0400

    Added JsonIgnore

--- src/main/java/com/revature/entities/Curriculum.java
+++ src/main/java/com/revature/entities/Curriculum.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Curriculum {
