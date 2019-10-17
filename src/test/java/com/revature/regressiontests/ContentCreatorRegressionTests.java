422ffd42bb169dc0445163a864fd8ec603ee7f06
Anthony <avisuano15@gmail.com>
2019-10-17 15:52:36 -0500

    Created individual tests for each element on the page, minus the Module Filter input. Because of databinding, Selenium wasn't able to click on modules searched for.

--- src/test/java/com/revature/regressiontests/ContentCreatorRegressionTests.java
+++ src/test/java/com/revature/regressiontests/ContentCreatorRegressionTests.java
@@ -4,14 +4,16 @@ import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.revature.pageobjectmodel.ContentCreator;
 
-public class ContentRegressionTest {
+public class ContentCreatorRegressionTests {
 
 	public static WebDriver driver;
 	
@@ -26,30 +28,46 @@ public class ContentRegressionTest {
 	
 	@Test
 	public void confirmedContentCreatorPage() {
-		ContentCreator cc = new ContentCreator(driver);
 		assertEquals(driver.getCurrentUrl(), url);
 	}
-	
 	@Test
 	public void checkTitleBox() {
 		ContentCreator cc = new ContentCreator(driver);
 		cc.inputToTitleBox("Testing the selenium title box test");
 		assertEquals(driver.findElement(By.id("titleTextBox")), cc.titleBox);
 	}
-	
-	
 	@Test
 	public void checkUrlBox() {
 		ContentCreator cc = new ContentCreator(driver);
 		cc.inputToUrlBox("http://www.testnumber2.com");
 		assertEquals(driver.findElement(By.id("urlTextBox")), cc.urlBox);
 	}
-	
 	@Test
 	public void checkDescriptionBox() {
 		ContentCreator cc = new ContentCreator(driver);
 		cc.inputToDescriptionBox("A quality description would go here");
 		assertEquals(driver.findElement(By.id("exampleFormControlTextarea1")), cc.descriptionBox);
+	}
+	@Test
+	public void checkCodeRadioButton() {
+		Actions actions = new Actions(driver);
+		WebElement radio = driver.findElement(By.id("Code"));
+		actions.moveToElement(radio).click().perform();
+		driver.findElement(By.id("Code")).isSelected();
+	}
+	@Test
+	public void checkDocumentRadioButton() {
+		Actions actions = new Actions(driver);
+		WebElement radio = driver.findElement(By.id("Document"));
+		actions.moveToElement(radio).click().perform();
+		driver.findElement(By.id("Document")).isSelected();
+	}
+	@Test
+	public void checkPowerpointRadioButton() {
+		Actions actions = new Actions(driver);
+		WebElement radio = driver.findElement(By.id("Powerpoint"));
+		actions.moveToElement(radio).click().perform();
+		driver.findElement(By.id("Powerpoint")).isSelected();
 	}
 	
 	@AfterClass
