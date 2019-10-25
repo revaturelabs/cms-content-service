7bbe36ec116b21da5b2b2214f850c6ffaf38c245
unknown <brett.robinson996@gmail.com>
2019-10-25 14:18:36 -0500

    Added JavaDoc documentation to custom exception classes

--- src/main/java/com/revature/exceptions/InvalidSearchException.java
+++ src/main/java/com/revature/exceptions/InvalidSearchException.java
@@ -1,13 +1,16 @@
 package com.revature.exceptions;
 
+/**
+ * This custom exception will be thrown when invalid input is found when a search is made.
+ */
 public class InvalidSearchException extends RuntimeException {
 
-	/**
-	 * Exception will be thrown when an InvalidSearch is done.
-	 * @author 190617jta
-	 */
 	private static final long serialVersionUID = 8223426408754344315L;
 
+	/**
+	 * Passes a String message to the {@link RuntimeException RuntimeException} class's constructor
+	 * @param msg
+	 */
 	public InvalidSearchException(String msg) {
 		super(msg);
 	}
